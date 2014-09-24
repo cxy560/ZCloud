f8c4bef8af51f6f4a3cfcdc83cd6cfa7ee7c2257
cxy560 <cxy560@163.com>
2014-09-24 11:45:31 +0800

    add msg queue

---
+++ cust/zc/zc_message_queue.c
@@ -0,0 +1,83 @@
+/**
+******************************************************************************
+* @file     zc_messgage_queue.c
+* @authors  cxy
+* @version  V1.0.0
+* @date     10-Sep-2014
+* @brief    Msg Queue
+******************************************************************************
+*/
+
+#include <zc_message_queue.h>
+
+
+/*************************************************
+* Function: MSG_InitQueue
+* Description: 
+* Author: cxy 
+* Returns: 
+* Parameter: 
+* History:
+*************************************************/
+void MSG_InitQueue(MSG_Queue *pstruMsgQue)   
+{
+    u32 u32Index;
+    MSG_QueNode *pstrNode = NULL;
+    for (u32Index = 0; u32Index < MSG_QUEUE_MAXLEN + 1; u32Index++)
+    {
+        pstruMsgQue->struQueue[u32Index].pstruNext = pstrNode;
+        pstruMsgQue->struQueue[u32Index].pu8Msg = NULL;
+        pstrNode = &pstruMsgQue->struQueue[u32Index];
+    }
+    /*Init a loop queue*/
+    pstruMsgQue->struQueue[0].pstruNext = &pstruMsgQue->struQueue[MSG_QUEUE_MAXLEN];
+    
+    pstruMsgQue->pstruHead = &pstruMsgQue->struQueue[0];
+    pstruMsgQue->pstruTail = pstruMsgQue->pstruHead;
+    return;
+}
+
+/*************************************************
+* Function: MSG_PushMsg
+* Description: 
+* Author: cxy 
+* Returns: 
+* Parameter: 
+* History:
+*************************************************/
+u32 MSG_PushMsg(MSG_Queue *pstruMsgQueue, u8 *pu8Msg)
+{
+    /*tail next == head, queue is full*/
+    if (pstruMsgQueue->pstruTail->pstruNext == pstruMsgQueue->pstruHead)
+    {
+        return ZC_RET_ERROR;
+    }
+    pstruMsgQueue->pstruTail->pu8Msg = pu8Msg;
+    pstruMsgQueue->pstruTail = pstruMsgQueue->pstruTail->pstruNext;
+    return ZC_RET_OK;
+}
+
+/*************************************************
+* Function: MSG_PopMsg
+* Description: 
+* Author: cxy 
+* Returns: 
+* Parameter: 
+* History:
+*************************************************/
+u8* MSG_PopMsg(MSG_Queue *pstruMsgQueue)
+{
+    u8 *pu8Msg = NULL;
+    
+    /*head == tail, no msg*/
+    if (pstruMsgQueue->pstruHead == pstruMsgQueue->pstruTail)
+    {
+        return pu8Msg;
+    }
+    
+    pu8Msg = pstruMsgQueue->pstruHead->pu8Msg;
+    pstruMsgQueue->pstruHead->pu8Msg = NULL;
+    pstruMsgQueue->pstruHead = pstruMsgQueue->pstruHead->pstruNext;
+    
+    return pu8Msg;
+}
---
+++ src/zc/zc_message_queue.h
@@ -0,0 +1,43 @@
+/**
+******************************************************************************
+* @file     zc_message_queue.h
+* @authors  cxy
+* @version  V1.0.0
+* @date     10-Sep-2014
+* @brief    Message
+******************************************************************************
+*/
+
+#ifndef  __ZC_MESSAGE_QUEUE_H__ 
+#define  __ZC_MESSAGE_QUEUE_H__
+
+#include <zc_common.h>
+
+#define  MSG_QUEUE_MAXLEN  (10)
+
+typedef struct struMSG_QueNode{
+    struct struMSG_QueNode* pstruNext;
+    u8 *pu8Msg;
+}MSG_QueNode;
+
+typedef struct{
+    MSG_QueNode *pstruHead;
+    MSG_QueNode *pstruTail;
+    MSG_QueNode struQueue[MSG_QUEUE_MAXLEN+1];
+}MSG_Queue;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void MSG_InitQueue(MSG_Queue *pstruMsgQue);
+u32 MSG_PushMsg(MSG_Queue *pstruMsgQueue, u8 *pu8Msg);
+u8* MSG_PopMsg(MSG_Queue *pstruMsgQueue);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+/******************************* FILE END ***********************************/
+
